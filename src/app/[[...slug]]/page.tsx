ac65210557a7b388ceb1ff72777ce2accee5f736
BaiMeow <baishu2003@outlook.com>
2024-11-18 15:13:02 +0800

    Wrap useSearchParams with Suspense

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -1,6 +1,11 @@
 import { ClientOnly } from "./client";
+import { Suspense } from "react";
 export default function Page() {
-  return <ClientOnly />;
+  return (
+    <Suspense>
+      <ClientOnly />
+    </Suspense>
+  );
 }
 
 export function generateStaticParams() {
