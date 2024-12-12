1f1aec3fc268e76297eca867e98adbee6a1577ad
柏喵Sakura <38121125+BaiMeow@users.noreply.github.com>
2024-12-12 17:26:28 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
     output: 'standalone',
     distDir: './build', // Changes the build output directory to `./dist`.
+    experimental: {
+        serverActions: {
+            allowedOrigins: ["card.dn11.baimeow.cn"],
+        },
+    },
   }
    
   export default nextConfig
